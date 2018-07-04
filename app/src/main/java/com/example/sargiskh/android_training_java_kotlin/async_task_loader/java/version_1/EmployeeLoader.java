16233d5e480a19f68712b57ea6d113b2060ea730
Sargis Khlopuzyan <sargiskh@SARGISKH-394>
2018-07-04 17:07:21 +0400

    Async Task Loader with real request

--- app/src/main/java/com/example/sargiskh/android_training_java_kotlin/async_task_loader/java/version_1/EmployeeLoader.java
+++ app/src/main/java/com/example/sargiskh/android_training_java_kotlin/async_task_loader/java/version_1/EmployeeLoader.java
@@ -2,9 +2,17 @@ package com.example.sargiskh.android_training_java_kotlin.async_task_loader.java
 
 import android.content.Context;
 import android.support.v4.content.AsyncTaskLoader;
+import android.util.Log;
 
 import com.example.sargiskh.android_training_java_kotlin.async_task_loader.java.Employee;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,6 +29,60 @@ public class EmployeeLoader extends AsyncTaskLoader<List<Employee>> {
         list.add(new Employee("emp1", "Brahma"));
         list.add(new Employee("emp2", "Vishnu"));
         list.add(new Employee("emp3", "Mahesh"));
+
+        /**
+        HttpURLConnection urlConnection = null;
+        BufferedReader reader = null;
+        String jsonStr = null;
+        String line;
+
+        try {
+            URL url = new URL("https://itunes.apple.com/search?term=classic");
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+            // Read the input stream into a String
+            InputStream inputStream = urlConnection.getInputStream();
+            StringBuffer buffer = new StringBuffer();
+
+            if (inputStream == null) {
+                return null;
+            }
+
+            reader = new BufferedReader(new InputStreamReader(inputStream));
+
+            while ((line = reader.readLine()) != null) {
+                buffer.append(line);
+            }
+
+            if (buffer.length() == 0) {
+                return null;
+            }
+
+            jsonStr = buffer.toString();
+
+        } catch (MalformedURLException e) {
+            Log.e("MainActivity", "Error : MalformedURLException", e);
+            return null;
+        } catch (IOException e) {
+            Log.e("MainActivity", "Error : IOException", e);
+            return null;
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (IOException e) {
+                        Log.e("LOG_TAG", "Error closing stream", e);
+                    }
+                }
+            }
+        }
+
+        Log.e("LOG_TAG", "jsonStr: " + jsonStr);
+        */
 
         return list;
     }
--- app/src/main/java/com/example/sargiskh/android_training_java_kotlin/async_task_loader/java/version_2/MainActivity_Async_Task_Loader_Version_2.java
+++ app/src/main/java/com/example/sargiskh/android_training_java_kotlin/async_task_loader/java/version_2/MainActivity_Async_Task_Loader_Version_2.java
@@ -14,6 +14,13 @@ import com.example.sargiskh.android_training_java_kotlin.R;
 import com.example.sargiskh.android_training_java_kotlin.async_task_loader.java.Employee;
 import com.example.sargiskh.android_training_java_kotlin.async_task_loader.java.EmployeeAdapter;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -70,6 +77,60 @@ public class MainActivity_Async_Task_Loader_Version_2 extends AppCompatActivity 
             list.add(new Employee("emp1", "Brahma"));
             list.add(new Employee("emp2", "Vishnu"));
             list.add(new Employee("emp3", "Mahesh"));
+
+            /**
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+            String jsonStr = null;
+            String line;
+
+            try {
+                URL url = new URL("https://itunes.apple.com/search?term=classic");
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                // Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+
+                if (inputStream == null) {
+                    return null;
+                }
+
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line);
+                }
+
+                if (buffer.length() == 0) {
+                    return null;
+                }
+
+                jsonStr = buffer.toString();
+
+            } catch (MalformedURLException e) {
+                Log.e("MainActivity", "Error : MalformedURLException", e);
+                return null;
+            } catch (IOException e) {
+                Log.e("MainActivity", "Error : IOException", e);
+                return null;
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                    if (reader != null) {
+                        try {
+                            reader.close();
+                        } catch (IOException e) {
+                            Log.e("LOG_TAG", "Error closing stream", e);
+                        }
+                    }
+                }
+            }
+
+            Log.e("LOG_TAG", "jsonStr: " + jsonStr);
+            */
 
             return list;
         }
